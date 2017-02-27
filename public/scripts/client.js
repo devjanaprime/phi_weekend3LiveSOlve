7c137b1ded1825aa00ee1215b1f438ecbf82c1ef
devjanaprime <dev@primeacademy.io>
2017-02-27 14:43:16 -0600

    most of hard mode

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,26 +1,26 @@
 $( document ).ready( function(){
-  console.log( 'JQ' );
+  var num0 = '';
+  var num1 = '';
+  var operator = '';
+  var captureNum1 = false;
 
   $( '#clearButton' ).on( 'click', function(){
-    // empty our inputs
-    $( '#num0' ).val('');
-    $( '#num1' ).val('');
+    // set defaults
+    num0 = '';
+    num1 = '';
+    operator = '';
+    captureNum1 = false;
     // clear answer
     $( '#outputP' ).text('');
   });
 
-  $( '.operatorButton' ).on( 'click', function(){
-    // what type of operator is this?
-    var myOperator = $( this ).data( 'operator' );
-    console.log( 'in operator button click', myOperator );
-    // get user number Inputs
-    var num0 = $( '#num0' ).val();
-    var num1 = $( '#num1' ).val();
+  $( '#equalsButton' ).on( 'click', function(){
+    console.log( 'in equalsButton on click' );
     // assemble the object to send to the server
     var objectToSend = {
       x: num0,
       y: num1,
-      type: myOperator
+      type: operator
     }; // end objectToSend
     console.log( 'sending:', objectToSend );
     $.ajax({
@@ -34,5 +34,32 @@ $( document ).ready( function(){
         $( '#outputP' ).text( response.answer );
       }
     }); // end ajax
+  }); // end equalsButton on click
+
+  $( '.operatorButton' ).on( 'click', function(){
+    // what type of operator is this?
+    var myOperator = $( this ).data( 'operator' );
+    console.log( 'in operator button click', myOperator );
+    operator = myOperator;
+    // is num0 != ''
+    if( num0 != '' ){
+      // time to start capturing num1
+      captureNum1 = true;
+    }
   }); // end operator button click
+
+  $( '.numberButton' ).on( 'click', function(){
+    // get my number
+    var myNumber = $( this ).data( 'number' );
+    console.log( 'in numberButton on click', myNumber );
+    // concatenate on to existing number
+    if( captureNum1 ){
+      num1 = num1 + myNumber;
+    }
+    else{
+      num0 = num0 + myNumber;
+    }
+    console.log( 'num0:', num0, 'num1:', num1 );
+    // it's Luke's fault that I'm hungry, nom init
+  }); // end numberButton on click
 }); // end doc ready
